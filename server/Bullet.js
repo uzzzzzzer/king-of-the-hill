201e86f92f4fc24e5d95993dd87db3595596c820
uzzzzzzer <79088180+uzzzzzzer@users.noreply.github.com>
2022-09-06 14:29:11 +0300

    Update Bullet.js

--- server/Bullet.js
+++ server/Bullet.js
@@ -56,7 +56,7 @@ class Bullet extends Entity {
     const distanceStep = Vector.scale(this.velocity, deltaTime)
     this.position.add(distanceStep)
     this.distanceTraveled += distanceStep.mag2
-    //console.log(this.distanceTraveled , Constants.BULLET_MAX_TRAVEL_DISTANCE_SQ)
+    console.log(this.distanceTraveled , Constants.BULLET_MAX_TRAVEL_DISTANCE_SQ)
     if (this.inWorld() || this.distanceTraveled > Constants.BULLET_MAX_TRAVEL_DISTANCE_SQ) {
       this.destroyed = true
     }
