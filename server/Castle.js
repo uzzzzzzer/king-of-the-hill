1cd1302902d3b4cbf829fe5caf768af45f290708
uzzzzzzer <79088180+uzzzzzzer@users.noreply.github.com>
2022-09-07 10:19:28 +0300

    Create Castle.js

---
+++ server/Castle.js
@@ -0,0 +1,40 @@
+/**
+ * This class stores the state of a castle on the server.
+ */
+
+const Constants = require('../lib/Constants')
+const Entity = require('../lib/Entity')
+const Vector = require('../lib/Vector')
+
+/**
+ * additional object class.
+ */
+class Castle extends Entity {
+  /**
+   * Constructor for a castle.
+   * @constructor
+   * @param {Vector} position The starting position vector
+   * @param {Vector} velocity The starting velocity vector
+   * @param {number} angle The orientation of the bullet
+   * @param {string} type The type of this object
+   */
+  constructor(position, velocity, angle, owner, hitbox_size) {
+    super(position, velocity, Vector.zero(), hitbox_size)
+    this.owner = owner
+    this.type = type
+    this.destroyed = false
+  }
+
+
+  /**
+   * Performs a physics update.
+   * @param {number} lastUpdateTime The last timestamp an update occurred
+   * @param {number} deltaTime The timestep to compute the update with
+   */
+  update(lastUpdateTime, deltaTime) {
+    const distanceStep = Vector.scale(this.velocity, deltaTime)
+    this.position.add(distanceStep)
+  }
+}
+
+module.exports = Castle
