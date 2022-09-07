809e35e225db607d305709126b1ed35298b28137
uzzzzzzer <79088180+uzzzzzzer@users.noreply.github.com>
2022-09-07 18:45:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -45,14 +45,15 @@ app.get('/', (request, response) => {
 io.on('connection', socket => {
   socket.on(Constants.SOCKET_NEW_PLAYER, (data, callback) => {
     let game = games[games.length - 1]
-    game.addNewPlayer(data.name, socket)
+    let nm = data.name + "(" + [...game.players.values()].length + ")"
+    game.addNewPlayer(nm, socket)
     sockets.set(socket.id, games.length - 1)
     if([...game.players.values()].length >= Constants.PLAYERS_IN_ROOM){
       games.push(new Game())
     }
     game.clients.forEach((client, socketID) => {game.clients.get(socketID).emit(Constants.SOCKET_CHAT_SERVER_CLIENT, {
       name: CHAT_TAG,
-      message: `${data.name} has joined the game.`,
+      message: `${nm} has joined the game.`,
       isNotification: true
     })
     })
