c7a208389e52fce8f3a15467dca041ddd23c153e
uzzzzzzer <79088180+uzzzzzzer@users.noreply.github.com>
2022-09-07 21:32:02 +0300

    Update Game.js

--- server/Game.js
+++ server/Game.js
@@ -81,9 +81,9 @@ class Game {
   }
   checkIfFull(){
     const currentTime = Date.now()
-    this.deltaTime += currentTime - this.lastUpdateTime
-    console.log(this.deltaTime)
+    this.deltaTime = currentTime - this.lastUpdateTime
     this.waitTime += this.deltaTime
+    console.log(this.waitTime)
     this.full = [...this.players.values()].length >= (Constants.PLAYERS_IN_ROOM - Constants.MAX_BOTS * (this.waitTime >= Constants.MAX_WAIT_TIME))
     if(this.full){
       console.log(this.waitTime, Constants.MAX_WAIT_TIME)
